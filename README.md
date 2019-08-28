6f7c4f356696a4f07022c28bdf6356d87782a59b
Krishan Kumar <krishan97@users.noreply.github.com>
2019-08-28 16:12:27 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Build-dynamic-JSON-based-form-in-reactJs
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import DynamicForm from './components/dynamic-form'
+function App() {
+  return (
+    <>
+      <DynamicForm />
+    </>
+  )
+}
+
+export default App
